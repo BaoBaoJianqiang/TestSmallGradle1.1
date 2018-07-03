73f323dbe2dad93e527dda345d99634cc1745608
baobaojianqiang <16230091@qq.com>
2018-07-03 21:53:02 +0800

    1v

---
+++ buildSrc/src/main/groovy/com/jianqiang/MyPlugin.groovy
@@ -0,0 +1,16 @@
+package com.jianqiang
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class MyPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        project.extensions.create('pluginSrc', MyExtension)
+
+        project.task('testPlugin') << {
+            println project.pluginSrc.message
+        }
+    }
+}
